650ed8b6cdcb1fc45f3e86f9ee341518be45c14c
Crowbcat <solopovminer@gmail.com>
2019-11-12 15:59:16 +0200

    added libft

---
+++ libft/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asolopov <asolopov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 11:24:37 by asolopov          #+#    #+#             */
+/*   Updated: 2019/10/22 12:56:35 by asolopov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		printf("1) FT_ATOI: %d\n", ft_atoi(argv[1]));
+		//printf("1) ATOI: %d", atoi(argv[1]));
+		//printf("2) FT_ISALNUM: %d\n", ft_atoi(argv[1]));
+	}
+	// if (argc == 3)
+	// {
+
+	// }
+	// if (argc == 4)
+	// {
+
+	// }
+}
