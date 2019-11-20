492bcaff76ad01ffad1a9e34540e6affba994544
Crowbcat <solopovminer@gmail.com>
2019-11-20 16:05:55 +0200

    adda

--- srcs/init_reset.c
+++ srcs/init_reset.c
@@ -6,7 +6,7 @@
 /*   By: asolopov <asolopov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 11:56:03 by asolopov          #+#    #+#             */
-/*   Updated: 2019/11/18 19:20:10 by asolopov         ###   ########.fr       */
+/*   Updated: 2019/11/20 16:05:12 by asolopov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void		reset_mprop(t_mprop *mprop)
 {
 	mprop->zoom = 15;
 	mprop->zmod = 2;
-	mprop->strtx = 850;
-	mprop->strty = 250;
+	mprop->strtx = MAP_WID / 4;
+	mprop->strty = MAP_LEN / 4;
 	mprop->eye->ex = -500;
 	mprop->eye->ey = -500;
 	mprop->eye->ez = 4000;
@@ -30,8 +30,8 @@ static void	init_mprop(t_mprop *mprop)
 	mprop->nlines = 0;
 	mprop->zoom = 15;
 	mprop->zmod = 2;
-	mprop->strtx = 850;
-	mprop->strty = 250;
+	mprop->strtx = MAP_WID / 4;
+	mprop->strty = MAP_LEN / 4;
 	mprop->perspective = 0;
 	mprop->mlx_ptr = mlx_init();
 	mprop->win_ptr = mlx_new_window(mprop->mlx_ptr, MAP_WID, MAP_LEN, "FdF");
